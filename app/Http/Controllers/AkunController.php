18ec82eb232e266c99023e2131ff2b9caeeaf72c
J0key <shyraathaya@mail.ugm.ac.id>
2024-03-20 14:45:33 +0700

    first commit

---
+++ app/Http/Controllers/AkunController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AkunController extends Controller
+{
+    public function view(){
+        return view('layout.main');
+    }
+
+    public function create(){
+        return view('create');
+    }
+}
---
+++ resources/views/create.blade.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>testing</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet" />
+</head>
+
+<body class="text-white bg-[#EFEFEF]">
+
+    {{-- sidebar start --}}
+    <div class="fixed w-64 bg-[#4C8F8B] left-0 top-0 h-full p-6">
+        <a href="" class="flex items-center">
+            <img src="https://placehold.co/40x40" alt="" class="w-10 h-10 rounded object-cover">
+            <span class="font-bold text-lg ml-3">TA - TRPL</span>
+        </a>
+        <ul class="mt-10">
+            <li class="group active">
+                <a href="#" class="flex items-center px-2 py-2 hover:bg-[#5B9692] rounded-md">
+                    <i class="ri-home-line pr-4"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="flex items-center px-2 py-5">
+                    <i class="ri-notification-2-line pr-4"></i>
+                    <span>Pengumuman</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="flex items-center px-2 py-5">
+                    <i class="ri-group-fill pr-4"></i>
+                    <span>User</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="flex items-center px-2 py-5">
+                    <i class="ri-user-line pr-4"></i>
+                    <span>Account</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="flex items-center px-2 py-5">
+                    <i class="ri-file-chart-line pr-4"></i>
+                    <span>Berkas</span>
+                </a>
+            </li>
+
+        </ul>
+
+
+    </div>
+    {{-- sidebar end --}}
+
+    {{-- main start --}}
+    <main class="w-[1139px] ml-[322px] min-h-screen my-10 ">
+        {{-- navbar start --}}
+        <div class="py-4 px-6 bg-[#FBFFFC] flex item-center shadow-md rounded-md shadow-black/5">
+            <ul class="ml-4 flex items-center">
+                <li class="mr-4">
+                    <button type="button"
+                        class="text-[#4C8F8B] w-8 h-8 rounded flex items-center justify-center hover:bg-gray-50 hover:text-gray-600">
+                        <i class="ri-search-line mr-4"></i>
+                        Search
+                    </button>
+                </li>
+            </ul>
+            <ul class="ml-auto flex items-center">
+                <li class="mr-1">
+                    <button type="button"
+                        class="text-[#4C8F8B] w-8 h-8 rounded flex items-center justify-center hover:bg-gray-50 hover:text-gray-600">
+                        <i class="ri-sun-line"></i>
+                    </button>
+                </li>
+                <li class="mr-1">
+                    <button type="button"
+                        class="text-[#4C8F8B] w-8 h-8 rounded flex items-center justify-center hover:bg-gray-50 hover:text-gray-600">
+                        <i class="ri-notification-4-fill"></i>
+                    </button>
+                </li>
+                <li class="mr-1">
+                    <button type="button"
+                        class="text-[#4C8F8B] w-8 h-8 rounded flex items-center justify-center hover:bg-gray-50 hover:text-gray-600">
+                        <i class="ri-account-circle-line"></i>
+
+                    </button>
+                </li>
+            </ul>
+        </div>
+        {{-- navbar end --}}
+
+
+
+
+
+
+    </main>
+    {{-- main end --}}
+
+</body>
+
+</html>
